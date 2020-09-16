a82681f682fb98bd310faf8b52d2b584f25ad94c
tekihei2317 <tekihei2317@gmail.com>
2020-09-16 18:21:44 +0900

    feat: ユーザー登録機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user=User.new
+  end
+
+  def create
+    # binding.pry
+
+    name=params[:user][:name]
+    email=params[:user][:email]
+    @user=User.new(name: name, email: email)
+
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+
+  get 'pages/index'
   get 'pages/help'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
 end
