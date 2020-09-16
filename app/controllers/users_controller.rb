90925a53cce292c2c4d97d236feb00cced978c80
tekihei2317 <tekihei2317@gmail.com>
2020-09-16 22:51:29 +0900

    feat: name, email, passwordのバリデーションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user=User.new(user_params)
-    # binding.pry
+    byebug
 
     if @user.save
       redirect_to root_path, success: "登録が完了しました(ユーザー名: #{@user.name})"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+
+  VALID_EMAIL_REGEX=/.+@.+\..+/
   validates :email, presence: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+
+  validates :password, length: { in: 8..32 }
+
 end
