308800314b450dfa30b2c19ddfdc67b4d5c681a7
tekihei2317 <tekihei2317@gmail.com>
2020-09-17 15:23:17 +0900

    feat: sessionsコントローラーにストロングパラメーターを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
-
+  
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # binding.pry
-    user=User.find_by(email: params[:session][:email])
-    if user&&user.authenticate(params[:session][:password])
+    binding.pry
+    user=User.find_by(email: session_params[:email])
+    if user&&user.authenticate(session_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインしました'
     else
@@ -20,6 +20,10 @@ class SessionsController < ApplicationController
   end
 
   private
+  
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 
   def log_in(user)
     session[:user_id]=user.id
