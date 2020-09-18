03a686d0fd46cf3cbceea41a0dd74e1a305c58ec
tekihei2317 <tekihei2317@gmail.com>
2020-09-18 15:22:31 +0900

    feat: 投稿一覧にコメントを表示

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :topic
   
-  validate :user_id, presence: true
-  validate :topic_id, presence: true
-  validate :content, presence: true
+  validates :user_id, presence: true
+  validates :topic_id, presence: true
+  validates :content, presence: true
 end
