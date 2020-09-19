5854d65e7453d16566f212999909951c04da470e
tekihei2317 <tekihei2317@gmail.com>
2020-09-19 12:35:32 +0900

    fix: マイグレーションをPostgreSQL用に変更

--- db/migrate/20200919030035_change_id_data_to_favorites.rb
+++ db/migrate/20200919030035_change_id_data_to_favorites.rb
@@ -1,6 +1,11 @@
 class ChangeIdDataToFavorites < ActiveRecord::Migration[5.2]
-  def change
-    change_column :favorites, :user_id, :integer
-    change_column :favorites, :topic_id, :integer
+  def up
+    change_column :favorites, :user_id, :'integer USING user_id::integer'
+    change_column :favorites, :topic_id, :'integer USING user_id::integer'
+  end
+
+  def down
+    change_column :favorites, :user_id, :string
+    change_column :favorites, :topic_id, :string
   end
 end
--- db/migrate/20200919030216_change_id_data_to_comments.rb
+++ db/migrate/20200919030216_change_id_data_to_comments.rb
@@ -1,6 +1,11 @@
 class ChangeIdDataToComments < ActiveRecord::Migration[5.2]
-  def change
-    change_column :comments, :user_id, :integer
-    change_column :comments, :topic_id, :integer
+  def up
+    change_column :comments, :user_id, :'integer USING user_id::integer'
+    change_column :comments, :topic_id, :'integer USING user_id::integer'
+  end
+
+  def down
+    change_column :comments, :user_id, :string
+    change_column :comments, :topic_id, :string
   end
 end
